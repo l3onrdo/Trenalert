73cfd9a50be5416e866e130daaec28bdf31beebe
l3onrdo <101908529+l3onrdo@users.noreply.github.com>
2022-12-23 18:38:25 +0100

    Update NotificaTelegram.php

--- NotificaTelegram.php
+++ NotificaTelegram.php
@@ -1,7 +1,7 @@
 <?php
-    $api_key = "5860264964:AAGI3l31kWylDKr2Xi533ai0AYUOVEOcqnI"; // Token del tuo bot
+    $api_key = "IL TUO TOKEN"; // Token del tuo bot
 
-    $chat_id = "695635146";//id della chat
+    $chat_id = "IL TUO ID";//id della chat
 
     // Invia un messaggio di testo alla chat
     $text = "Problemi sulla linea ecco le informazioni: \n\n".$string."\n\nPer ottenere alte informazioni contralli sul sito:\nhttps://www.trenitalia.com/it/informazioni/Infomobilita/notizie-infomobilita.html"; // Testo del messaggio
@@ -9,4 +9,4 @@
     $send_message = "https://api.telegram.org/bot" . $api_key . "/sendMessage?chat_id=" . $chat_id . "&text=" . urlencode($text);
 
     $response = file_get_contents($send_message);
-?>
+?>
