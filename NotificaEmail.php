21631a4329f280970bd113779d3c8ec981f9d725
l3onrdo <101908529+l3onrdo@users.noreply.github.com>
2022-12-23 18:39:17 +0100

    Update NotificaEmail.php

--- NotificaEmail.php
+++ NotificaEmail.php
@@ -1,12 +1,8 @@
 <?php
-$to = "leomiralli@gmal.com";
+$to = "Destinatario email";
 $subject = "Oggetto dell'email";
 $message = "Il corpo del messaggio dell'email";
 $headers = "Da: trenalert@gmail.com\r\n";
 
-if (mail($to, $subject, $message, $headers)) {
-  echo "L'email è stata inviata con successo!";
-} else {
-  echo "C'è stato un errore nell'invio dell'email.";
-}
+mail($to, $subject, $message, $headers);
 ?>
