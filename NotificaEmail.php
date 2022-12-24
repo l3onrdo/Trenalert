245b00e1fc889db28896186388929ff3b6a3f62a
l3onrdo <101908529+l3onrdo@users.noreply.github.com>
2022-12-24 18:12:12 +0100

    Update NotificaEmail.php

--- NotificaEmail.php
+++ NotificaEmail.php
@@ -1,7 +1,7 @@
 <?php
 $to = "Destinatario email";
 $subject = "Oggetto dell'email";
-$message = "Il corpo del messaggio dell'email";
+$message = $text = "Problemi sulla linea ecco le informazioni: \n\n".$string."\n\nPer ottenere alte informazioni contralli sul sito:\nhttps://www.trenitalia.com/it/informazioni/Infomobilita/notizie-infomobilita.html";
 $headers = "Da: trenalert@gmail.com\r\n";
 
 mail($to, $subject, $message, $headers);
